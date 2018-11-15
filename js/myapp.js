eee371d123aee9793b9bc82c0653c581d0b87614
JrmSK <jeremy.skornik@gmail.com>
2018-11-15 20:51:07 +0200

    Cards are generated with JS. Updated pictures library

--- js/myapp.js
+++ js/myapp.js
@@ -1,20 +1,27 @@
 $(document).ready(function () {
-  var firstTime = true;
-  var initiate = true;
-  var startTime;
-  var endTime;
 
-  $("#btn").on("click", function () {
-    if (firstTime) {
-      startTime = new Date();
-      firstTime = false;
-    } else {
-      initiate = false;
-      endTime = new Date();
-      firstTime = true;
-    }
-    if (!initiate) {
-      endTime > startTime ? $("#display").text(`Last time you clicked was ${endTime - startTime} ms ago`) : $("#display").text(`Last time you clicked was ${startTime - endTime} ms ago`);
-    }
-  });
 });
+
+var MemoryGame = {};
+MemoryGame.images = ["pic0.jpg", "pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg", "pic6.jpg", "pic7.jpg", "pic8.jpg", "pic9.jpg",];    // if you want to add an image, add it here (keep same format, and try to keep ratio as well) 
+MemoryGame.backCard = "";
+
+MemoryGame.generateBoard = function () {
+    var cardsByRow = 4;                 // change here when dealing with difficulty
+    for (var i = 1; i <= 3; i++) {
+        $("#board").append("<div class='row justify-content-center'>");
+        for (var j = 1; j <= cardsByRow; j++) {
+            $(`.row:nth-child(${i})`).append("<div class='col-xs-3 card'>");
+        }
+    }
+
+}
+
+
+
+
+MemoryGame.start = function () {
+    MemoryGame.generateBoard();
+}
+
+MemoryGame.start();
